cae6ee9826938eacc86ea7f80ba2e9a81df41d4e
Jordan Stomps <stomps@wisc.edu>
2021-04-12 17:56:07 -0500

    adding broken unit test for RadClass

--- tests/create_file.py
+++ tests/create_file.py
@@ -1,18 +1,19 @@
 import h5py
+import numpy as np
 
 def create_file(filename, datapath, labels, live, timestamps, spectra):
     # Creating sample dataset
     f = h5py.File(filename, "w")
     
     # data structure for MUSE files
-    dset1 = f.create_dataset(datapath + labels['live'], (1000,))
-    dset2 = f.create_dataset(datapath + labels['timestamps'], (1000,))
-    dset3 = f.create_dataset(datapath + labels['spectra'], (1000,1000))
+    dset1 = f.create_dataset(datapath + labels['live'], (1000,), data=live)
+    dset2 = f.create_dataset(datapath + labels['timestamps'], (1000,),data=timestamps)
+    dset3 = f.create_dataset(datapath + labels['spectra'], (1000,1000),data=spectra)
 
     # store randomized data in test file
-    dset1[...] = live
-    dset2[...] = timestamps
-    dset3[...] = spectra
+    #dset1[...] = live
+    #dset2[...] = timestamps
+    #dset3[...] = spectra
 
     # close test file
     f.close()
