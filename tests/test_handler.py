a13f397d673e96d0e2b3fba82dc6bbc50d28150c
Jordan Stomps <stomps@wisc.edu>
2021-06-15 13:44:58 -0500

    adding init and analysis tests

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -5,11 +5,56 @@ from datetime import datetime, timedelta
 from RadClass.RadClass import RadClass
 import tests.create_file as file
 
+class NullAnalysis():
+    changed = False
+    
+    # tracks whether this class is called properly
+    def run(self, data):
+        self.changed = True
+
+def test_analysis():
+    # randomized data to store (smaller than an actual MUSE file)
+    live = np.random.rand(file.timesteps,)
+    timestamps = np.random.rand(file.timesteps,)
+    spectra = np.random.rand(file.timesteps, file.energy_bins)
+
+    # create sample test file with above simulated data
+    file.create_file(live, timestamps, spectra)
+
+    stride = 60
+    integration = 60
+
+    # run handler script
+    classifier = RadClass(stride, integration, file.datapath, file.filename,
+                          analysis = NullAnalysis(), store_data=True)
+    classifier.run_all()
+
+    np.testing.assert_equal(True, classifier.analysis.changed)
+
+def test_init():
+    stride = 60
+    integration = 60
+    store_data = True
+    cache_size = 10000
+
+    classifier = RadClass(stride = stride, integration = integration,
+                          datapath = file.datapath, filename = file.filename,
+                          store_data=store_data, cache_size=cache_size,
+                          labels = file.labels)
+
+    np.testing.assert_equal(stride, classifier.stride)
+    np.testing.assert_equal(integration, classifier.integration)
+    np.testing.assert_equal(file.datapath, classifier.datapath)
+    np.testing.assert_equal(file.filename, classifier.filename)
+    np.testing.assert_equal(store_data, classifier.store_data)
+    np.testing.assert_equal(cache_size, classifier.cache_size)
+    np.testing.assert_equal(file.labels, classifier.labels)
+
 
 def test_integration():
+    # initialize sample data
     start_date = datetime(2019, 2, 2)
     delta = timedelta(seconds=1)
-
     timestamps = np.arange(start_date, start_date + (file.timesteps * delta), delta).astype('datetime64[s]').astype('float64')
 
     live = np.full((len(timestamps),), file.livetime)
