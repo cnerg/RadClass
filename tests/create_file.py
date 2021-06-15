3f35fe0dea9ace0527594624234ed41d501b225a
Jordan Stomps <stomps@wisc.edu>
2021-06-15 13:50:35 -0500

    pep8

--- tests/create_file.py
+++ tests/create_file.py
@@ -16,12 +16,12 @@ def create_file(live, timestamps, spectra):
     f = h5py.File(filename, "w")
 
     # data structure for MUSE files
-    dset1 = f.create_dataset(datapath + labels['live'], (timesteps,),
-                             data=live, dtype='float64')
-    dset2 = f.create_dataset(datapath + labels['timestamps'], (timesteps,),
-                             data=timestamps, dtype='float64')
-    dset3 = f.create_dataset(datapath + labels['spectra'], (timesteps, energy_bins),
-                             data=spectra, dtype='float64')
+    f.create_dataset(datapath + labels['live'], (timesteps,),
+                     data=live, dtype='float64')
+    f.create_dataset(datapath + labels['timestamps'], (timesteps,),
+                     data=timestamps, dtype='float64')
+    f.create_dataset(datapath + labels['spectra'], (timesteps, energy_bins),
+                     data=spectra, dtype='float64')
 
     # close test file
     f.close()
--- tests/test_RadClass.py
+++ tests/test_RadClass.py
@@ -5,12 +5,14 @@ from datetime import datetime, timedelta
 from RadClass.RadClass import RadClass
 import tests.create_file as file
 
+
 class NullAnalysis():
     changed = False
-    
+
     # tracks whether this class is called properly
     def run(self, data):
         self.changed = True
+
 
 def test_analysis():
     # randomized data to store (smaller than an actual MUSE file)
@@ -26,10 +28,11 @@ def test_analysis():
 
     # run handler script
     classifier = RadClass(stride, integration, file.datapath, file.filename,
-                          analysis = NullAnalysis(), store_data=True)
+                          analysis=NullAnalysis(), store_data=True)
     classifier.run_all()
 
     np.testing.assert_equal(True, classifier.analysis.changed)
+
 
 def test_init():
     stride = 60
@@ -37,10 +40,10 @@ def test_init():
     store_data = True
     cache_size = 10000
 
-    classifier = RadClass(stride = stride, integration = integration,
-                          datapath = file.datapath, filename = file.filename,
+    classifier = RadClass(stride=stride, integration=integration,
+                          datapath=file.datapath, filename=file.filename,
                           store_data=store_data, cache_size=cache_size,
-                          labels = file.labels)
+                          labels=file.labels)
 
     np.testing.assert_equal(stride, classifier.stride)
     np.testing.assert_equal(integration, classifier.integration)
@@ -55,10 +58,12 @@ def test_integration():
     # initialize sample data
     start_date = datetime(2019, 2, 2)
     delta = timedelta(seconds=1)
-    timestamps = np.arange(start_date, start_date + (file.timesteps * delta), delta).astype('datetime64[s]').astype('float64')
+    timestamps = np.arange(start_date, start_date + (file.timesteps * delta),
+                           delta).astype('datetime64[s]').astype('float64')
 
     live = np.full((len(timestamps),), file.livetime)
-    spectra = np.full((len(timestamps), file.energy_bins), np.full((1, file.energy_bins), 10.0))
+    spectra = np.full((len(timestamps), file.energy_bins),
+                      np.full((1, file.energy_bins), 10.0))
 
     # create sample test file with above simulated data
     file.create_file(live, timestamps, spectra)
