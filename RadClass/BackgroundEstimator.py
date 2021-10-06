2a74b7e38b2b98c4434d890bb050a001b867e51d
Jordan Stomps <stomps@wisc.edu>
2021-10-06 09:00:01 -0500

    remodeling BackgroundEstimator to exclude store_all

--- RadClass/BackgroundEstimator.py
+++ RadClass/BackgroundEstimator.py
@@ -16,7 +16,6 @@ class BackgroundEstimator:
     confidence: Percentage of samples to disregard. 1-confidence number of
         samples are saved. For example, if confidence=0.95, the lowest 5% of
         count rates/timestamps are saved.
-    store_all: Boolean. Indicates whether to store spectral information.
     spectra: Optional NumPy array that stores spectral data from energy bins.
     energy_bins: The number of energy bins defined by the data under analysis.
     '''
@@ -24,31 +23,8 @@ class BackgroundEstimator:
     background = pd.DataFrame(columns=['timestamp', 'count_rate'])
     data = np.empty((0, 2))
 
-    def __init__(self, confidence=0.95,
-                 store_all=False, energy_bins=1000):
+    def __init__(self, confidence=0.95):
         self.confidence = confidence
-        self.energy_bins = energy_bins
-
-        self.store_all = store_all
-        if store_all:
-            self.store_all = store_all
-            self.spectra = np.empty((0, energy_bins))
-
-    '''
-    def __del__(self):
-        self.background = pd.DataFrame(columns=['timestamp', 'count_rate'])
-        self.estimate()
-        # this method could be compressed to save memory
-        self.background.to_csv(self.ofilename+'.csv', index=False)
-    '''
-
-    def save_all(self):
-        '''
-        Adds spectra to saved data if requested.
-        '''
-
-        for i in range(self.energy_bins):
-            self.background[str(i+1)] = self.spectra[:, i]
 
     def sort(self):
         '''
@@ -62,18 +38,16 @@ class BackgroundEstimator:
         '''
         Saves data to a Pandas DataFrame and selects a subset with the smallest
             gross count rates.
+        If not writing to file, this must be called manually to prune samples.
 
         Attributes:
         samples_size: The total number of samples analyzed.
         num_samples: The number of samples to select and save.
         '''
+
         # building pandas DataFrame once from numpy array
         self.background['timestamp'] = self.data[:, 0]
         self.background['count_rate'] = self.data[:, 1]
-
-        # must happen before estimation to maintain order of rows
-        if self.store_all:
-            self.save_all()
 
         # find number of background samples
         sample_size = len(self.background.index)
@@ -95,32 +69,19 @@ class BackgroundEstimator:
 
         count_rate = np.sum(data)
 
-        if self.store_all:
-            self.spectra = np.vstack((self.spectra, data))
-
-        # alternative: use pandas and "append"
-        # slower (by 3 and 20 minutes respectively) than storing in array
-        # dfrow = {'timestamp': timestamp, 'count-rate': count_rate}
-        # self.background.loc[len(self.background.index)] = dfrow
-        # self.background = pd.concat([self.background,
-        #                              pd.DataFrame([data],
-        #                                           index=[timestamp])])
-
         # using numpy to build lightweight matrix instead of pandas
         self.data = np.vstack([self.data, [timestamp, count_rate]])
 
-        # instead of estimating at the end, checkpointing could occur
-        # if len(self.data) % 10000:
-        #    self.estimate()
-
     def write(self, ofilename='bckg_results'):
         '''
-        Identical to destructor but can be manually called.
-        NOTE: Only write or __del__ should be kept, but there is a design
-            question of which is the appropriate method to write to file.
+        Writes results to file.
+        Calls estimate() first to prune data array, only saving
+            (1-confidence)% of samples.
+
+        Attributes:
+        ofilename: string, exluding file extension, for filename to save.
         '''
 
-        # user must call estimate, or it could be part of write()
-        # self.estimate()
-        # this method could be compressed to save memory
+        # until estimate() is called, all background count-rates are saved
+        self.estimate()
         self.background.to_csv(ofilename+'.csv', index=False)
--- tests/test_BackgroundEstimator.py
+++ tests/test_BackgroundEstimator.py
@@ -27,15 +27,10 @@ def init_test_file():
 
 def test_init():
     confidence = 0.8
-    store_all = True
 
-    bckg = BackgroundEstimator(confidence=confidence,
-                               store_all=store_all,
-                               energy_bins=test_data.energy_bins)
+    bckg = BackgroundEstimator(confidence=confidence)
 
     np.testing.assert_equal(confidence, bckg.confidence)
-    np.testing.assert_equal(store_all, bckg.store_all)
-    np.testing.assert_equal(test_data.energy_bins, bckg.energy_bins)
 
 
 def test_estimation():
@@ -55,7 +50,9 @@ def test_estimation():
     #   counts * integration / live-time
     expected = (((integration-1)**2 + (integration-1)) /
                 (2*test_data.livetime)) * test_data.energy_bins / integration
-    np.testing.assert_almost_equal(bckg.background.iloc[0][1], expected, decimal=3)
+    np.testing.assert_almost_equal(bckg.background.iloc[0][1],
+                                   expected,
+                                   decimal=3)
 
     time_idx = np.where(spectra == 0)[0][0]
     np.testing.assert_equal(bckg.background.iloc[0][0], timestamps[time_idx])
@@ -65,26 +62,34 @@ def test_estimation():
     np.testing.assert_equal(len(bckg.background), expected_num)
 
 
-def test_spectral_storage():
+def test_write():
     stride = 10
     integration = 10
 
     confidence = 0.8
-    bckg = BackgroundEstimator(confidence=confidence,
-                               store_all=True,
-                               energy_bins=test_data.energy_bins)
+    bckg = BackgroundEstimator(confidence=confidence)
     # run handler script
     classifier = RadClass(stride, integration, test_data.datapath,
                           test_data.filename, store_data=True, analysis=bckg)
     classifier.run_all()
 
-    bckg.estimate()
-    bckg.write()
+    ofilename = 'bckg_test'
+    bckg.write(ofilename=ofilename)
 
-    expected = np.full((test_data.energy_bins,),
-                       ((integration-1)**2 + (integration-1)) /
-                       (2*test_data.livetime*integration))
-    results = np.genfromtxt('bckg_results.csv', delimiter=',', skip_header=1)
-    np.testing.assert_almost_equal(results[0][2:], expected, decimal=3)
+    results = np.genfromtxt(ofilename+'.csv', delimiter=',')[1:]
+    print(results)
 
-    os.remove('bckg_results.csv')
+    # the resulting observation should be:
+    #   counts * integration / live-time
+    expected = (((integration-1)**2 + (integration-1)) /
+                (2*test_data.livetime)) * test_data.energy_bins / integration
+    np.testing.assert_almost_equal(results[0][1], expected, decimal=3)
+
+    time_idx = np.where(spectra == 0)[0][0]
+    np.testing.assert_equal(results[0][0], timestamps[time_idx])
+
+    expected_num = round((test_data.timesteps / integration) *
+                         (1 - confidence))
+    np.testing.assert_equal(results.shape[0], expected_num)
+
+    os.remove(ofilename+'.csv')
