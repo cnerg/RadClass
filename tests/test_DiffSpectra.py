2b2499e1e32bd0c6b9f0924b89153b303671b69f
Jordan Stomps <stomps@wisc.edu>
2022-03-07 13:39:05 -0600

    completing unit test for DiffSpectra

--- tests/test_DiffSpectra.py
+++ tests/test_DiffSpectra.py
@@ -40,14 +40,27 @@ def test_difference():
     classifier.run_all()
 
     diff_spectra = post_analysis.diff_spectra
+    # DiffSpectra length test:
+    # there should be one difference spectrum for each timestamp with
+    # diff_stride number of spectra before it
+    exp_len = classifier.storage.shape[0] - diff_stride
+    np.testing.assert_equal(diff_spectra.shape[0], exp_len)
+
+    # DiffSpectra value test:
     # for test_data, the minimum background will always be the first spectrum
     # in a window (because the spectra increase 1, 2, 3, 4, etc.)
     # therefore the diff spectra element values will always be
     # spectra[i] - spectra[i-diff_stride]
-    # the algebra for n(n+1)/2 integrated intervals is simplified below:
+    # (the algebra for n(n+1)/2 integrated intervals is simplified below)
     diff_value = 3*stride**2 - integration**2 \
         + (2*stride*integration) + stride - integration
-    exp_spectra = np.full((classifier.storage.shape[0]-diff_stride,
-                           test_data.energy_bins),
+    exp_spectra = np.full((exp_len, test_data.energy_bins),
                           diff_value/(2*test_data.livetime))
-    np.testing.assert_almost_equal(diff_spectra[:, 1:], exp_spectra)
+    np.testing.assert_almost_equal(diff_spectra[:, 1:], exp_spectra, decimal=2)
+
+    # DiffSpectra timestamp test:
+    # there should be one difference spectrum for each timestamp with
+    # diff_stride number of spectra before it (i.e. spectra for every timestamp
+    # after the first diff_stride spectra)
+    exp_ts = classifier.storage[diff_stride:, 0]
+    np.testing.assert_equal(diff_spectra[:, 0], exp_ts)
