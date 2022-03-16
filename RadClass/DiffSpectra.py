18bd4847b3512a528072586f8e62f8b1d13272dc
Jordan Stomps <stomps@wisc.edu>
2022-03-15 19:28:57 -0500

    expanding comments for DiffSpectra.run

--- RadClass/DiffSpectra.py
+++ RadClass/DiffSpectra.py
@@ -18,12 +18,21 @@ class DiffSpectra:
     def run(self, data):
         # initializing spectra, with bckg to be subtracted below
         self.diff_spectra = data[self.stride:].copy()
-        # compute all integrated count-rates
+        # compute the gross integrated count-rate for every
+        # row/spectrum in the dataset
         sums = np.sum(data[:, 1:], axis=1)
+        # loop over every index in the difference spectra to be calculated
+        # that is, from self.stride to the end of the dataset
+        # (since those are the spectra with the requisite window
+        # of length self.stride before it)
         for i in range(self.diff_spectra.shape[0]):
-            # select the smallest spectrum from a window self.stride long
+            # from a window of sums (i.e. gross integrated count-rates)
+            # of length self.stride find the minimum
+            # select the smallest spectrum as bckg_spectrum
+            # sums includes all data [:] but bckg_spectrum is computed
+            # for [self.stride:], so indexing data is offset by i
             bckg_spectrum = data[np.argmin(sums[i:i+self.stride])+i]
-            # subtract background from stored spectrum
+            # subtract background from stored spectrum for the appropriate row
             self.diff_spectra[i, 1:] -= bckg_spectrum[1:]
 
     def write(self, filename):
