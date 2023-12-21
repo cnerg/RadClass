665566918179fd8371548accbf6dce3d8c5fa57e
Jordan Stomps <stomps@wisc.edu>
2023-12-21 16:31:05 -0500

    removing hardcoded license

--- RadClass/models/SSL/SimCLR.py
+++ RadClass/models/SSL/SimCLR.py
@@ -23,35 +23,6 @@ import joblib
 
 import logging
 
-'''
-Author: Jordan Stomps
-
-Largely adapted from a PyTorch conversion of SimCLR by Adam Foster.
-More information found here: https://github.com/ae-foster/pytorch-simclr
-
-MIT License
-
-Copyright (c) 2023 Jordan Stomps
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-'''
-
 '''Train an encoder using Contrastive Learning.'''
 
 
